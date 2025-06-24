095f64a5597a65f1383ea481a9623ffc106c93c5
hepticlove <44006470-hepticlove@users.noreply.replit.com>
2025-06-24 18:30:06 +0000

    Restored to 'ea5427b21ee4de772d30be4c31f10fc41055360e'
    Replit-Restored-To: ea5427b21ee4de772d30be4c31f10fc41055360e

--- server/index.ts
+++ server/index.ts
@@ -65,7 +65,6 @@ app.use(session({
     createTableIfMissing: false,
     tableName: 'sessions',
   }),
-  name: 'connect.sid',
   secret: process.env.SESSION_SECRET || 'change-this-secret-in-production',
   resave: false,
   saveUninitialized: false,
